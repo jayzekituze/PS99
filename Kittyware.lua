96eeb56ae15d7f5a28a1dd38e96ef4c5b170dcba
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-09-05 12:51:45 +0800

    561

--- Kittyware.lua
+++ Kittyware.lua
@@ -54,7 +54,7 @@ task.spawn(function()
         Inf_Pet_Speed = true,
 
         Event_Specific = {
-            Color_Gifts = {Enabled = true, Minimum_Buckets = 20},
+            Color_Gifts = {Enabled = true, Minimum_Buckets = 1},
             Clan_Points = {Enabled = false, Minimum_Buckets = 50},
             Stay_Teams = {Enabled = true, Only_Winning = true},
         },
