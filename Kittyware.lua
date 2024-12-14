b58e0ae3e1f4780be639b7dacec547cda5f0efc2
jayzekituze <102671903+jayzekituze@users.noreply.github.com>
2024-12-15 04:29:16 +0800

    b1235b

--- Kittyware.lua
+++ Kittyware.lua
@@ -5,45 +5,17 @@ print("utohatt")
 wait(10)
 --// loadstring(game:HttpGet('https://raw.githubusercontent.com/jayzekituze/PS99/main/Kittyware.lua'))()
 
-local function serverHop(id)
-    local HttpService = game:GetService("HttpService")
-    local TeleportService = game:GetService("TeleportService")
-    local Players = game:GetService("Players")
-    local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true"
-    local req = request({
-        Url = string.format(sfUrl, id, "Desc", 100)
-    })
-    local body = HttpService:JSONDecode(req.Body)
-    task.wait(0.2)
-    local servers = {}
-    if body and body.data then
-        for i, v in next, body.data do
-            if type(v) == "table" and v.playing >= 1 and v.id ~= game.JobId then
-                table.insert(servers, 1, v.id)
-            end
-        end
-    end
-    local randomCount = #servers
-    if not randomCount then
-        randomCount = 2
-    end
-    TeleportService:TeleportToPlaceInstance(id, servers[math.random(1, randomCount)], Players.LocalPlayer)
-end
-
-task.spawn(function()
-    print("Auto Reconnect Executed!")
-    game:GetService("GuiService").ErrorMessageChanged:Connect(function(errorMessage)       
-        game.Players.LocalPlayer:Kick("Found An Error, Reconnecting...")
-        print("Error message:", errorMessage)
-        print("Found An Error, Reconnecting...")
-        wait(0.1)
-        serverHop(8737899170) -- Attempt to teleport to another server
-    end);
-end)
-
 task.spawn(function()
     wait(10)
     print("hatsing")
     script_key="PzcFLuJeAvYmaetHLXDZASrmPjukMAEB";
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a2369ce34d44187610ac9bf5039c37ab.lua"))()
+
+    wait(60)
+    -- Get the player's character
+    local player = game.Players.LocalPlayer
+    local character = player.Character or player.CharacterAdded:Wait()
+
+    -- Teleport to the specified position
+    character.HumanoidRootPart.Position = Vector3.new(2113.27, 16.40, -20880.16)
 end)
